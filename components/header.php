43ae20a63ad44057258b3af20807310900136989
Pawel <pawel4985@gmail.com>
2023-11-27 23:02:20 +0100

    fix first commit

---
+++ components/header.php
@@ -0,0 +1,17 @@
+<header>
+    <img src="img/static/favicon.png" alt="Logo">
+    <nav>
+        <ul>
+            <li><a href="?page=announcements">Ogłoszenia</a></li>
+            <li><a href="?page=collections"><i class="fa-solid fa-hand-holding-heart"></i>Akcje</a></li>
+            <?php
+            if (isset($_SESSION['login'])) {
+                echo ('<li><a href="?page=account"><i class="fa-solid fa-user"></i>Twoje konto</a></li>');
+            } else {
+                echo ('<li><a href="?page=login"><i class="fa-solid fa-user"></i>Twoje konto</a></li>');
+            }
+            ?>
+            <li><a href="?page=addannouncement">Dodaj ogłoszenie</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ components/loginRegister.php
@@ -0,0 +1,22 @@
+<div class="forms">
+    <div class="control"><button class="click">Zaloguj się</button><button>Załóż konto</button></div>
+    <div class="inputs">
+        <div class="login">
+            <label for="email">E-mail</label>
+            <input type="email" name="email">
+            <label for="password">Hasło</label>
+            <input type="password" name="password">
+            <a href="#">Nie pamiętasz hasła?</a>
+            <button>Zaloguj się</button>
+        </div>
+        <div class="register hide">
+            <label for="email">E-mail</label>
+            <input type="email" name="email">
+            <label for="password">Hasło</label>
+            <input type="password" name="password">
+            <label for="password2">Powtórz hasło</label>
+            <input type="password" name="password2">
+            <button>Załóż konto</button>
+        </div>
+    </div>
+</div>
