8b163754966b9b7f66e56c77eb8b40b83267af99
Pawel <pawel4985@gmail.com>
2023-12-25 18:19:27 +0100

    add more categories and set categories layout

--- components/announcements.php
+++ components/announcements.php
@@ -17,7 +17,21 @@
         <i class="fa-solid fa-mobile-screen"></i>
         <p>Elektronika</p>
       </div>
+      <div class="category">
+        <i class="fa-solid fa-paint-roller"></i>
+        <p>Usługi</p>
+      </div>
+      <div class="category">
+        <i class="fa-solid fa-shirt"></i>
+        <p>Moda</p>
+      </div>
+      <div class="category">
+        <i class="fa-solid fa-calendar-days"></i>
+        <p>Wypożyczalnia</p>
+      </div>
     </div>
   </div>
-  <div class="offers"></div>
+  <div class="annoucments">
+    <h1>Ogłoszenia</h1>
+  </div>
 </div>
